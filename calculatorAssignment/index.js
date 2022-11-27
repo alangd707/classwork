96df19b8e503fe7a576ec4bd68ac38abc2b49b4c
alangd707 <111930791+alangd707@users.noreply.github.com>
2022-11-27 16:44:04 -0700

    Finished product
    - Few tiny bugs that make it a bit gunky such as in an equation if there are multiple operators with equal priority, it will operate the one that comes first and none, however not too bad of a fix, tiny and insignificant.

--- calculatorAssignment/index.js
+++ calculatorAssignment/index.js
@@ -1,23 +1,96 @@
 const display = document.getElementById('display');
-let displayValue = 'first';
-let firstValue = 0;
-let secondValue = null;
+let expression = []
+let positive = true;
+let decimal = false;
 
-function buttonClick(buttonType) {
-    if(typeof(buttonType) === 'number') {
-        firstValue = (firstValue * 10) + buttonType
-    } else if (buttonType === 'delete') {
-        if (displayValue === 'first') {
-            firstValue = Math.floor(firstValue * .1);
-        } else {
-            secondValue = Math.floor(sceondvalue * .1)
-        }
-    } else if (buttonType === 'all-clear') {
+
+function changeValue(value) {
+    if (Number(expression[expression.length - 1])) {
+        expression[expression.length - 1] += value
+    } else {
+        expression.push(value)
+    }
+    updateInterface()
+}
+
+function addDecimalPoint() {
+    if (Number(expression[expression.length - 1]) && !(expression[expression.length - 1].includes('.'))) {expression[expression.length - 1] += '.'}
+    else {
 
     }
-    updateDisplay()
+    updateInterface()
 }
 
-function updateDisplay(condition) {
-    display.innerHTML = firstValue
+function addOperator(operator) {
+    if (Number(expression[expression.length - 1])) {
+        expression.push(operator);
+        updateInterface();
+    }
 }
+
+function squareRoot() {
+    if (Number(expression[expression.length - 1])) {
+        expression[expression.length - 1] = Math.sqrt(expression[expression.length - 1])
+    }
+    updateInterface();
+}
+
+function deleteInput(type) {
+    if (type) {expression.pop()} else {expression = []}
+    updateInterface();
+}
+
+function negative() {
+    if (Number(expression[expression.length - 1])) {
+        if (!expression[expression.length - 1].includes('-')) {
+            expression[expression.length - 1] = '-' + expression[expression.length - 1]
+        } else {
+            expression[expression.length - 1].replace('-', '')
+        }
+    }
+    updateInterface()
+}
+
+function equals() {
+    if (Number(expression[expression.length - 1])) {
+        expression.forEach((y) => {
+            let initialIndex = expression.indexOf(y)
+            if (y === '^') {
+                expression[initialIndex] = "" + (Number(expression[initialIndex - 1]) ** Number(expression[initialIndex + 1]))
+                expression.splice(initialIndex + 1, 1)
+                expression.splice(initialIndex - 1, 1)
+            }
+        })
+        expression.forEach((y) => {
+            let initialIndex = expression.indexOf(y)
+            if (y === 'x') {
+                expression[initialIndex] = "" + (Number(expression[initialIndex - 1]) * Number(expression[initialIndex + 1]))
+                expression.splice(initialIndex + 1, 1)
+                expression.splice(initialIndex - 1, 1)
+            } else if (y === '/') {
+                expression[initialIndex] = "" + (Number(expression[initialIndex - 1]) / Number(expression[initialIndex + 1]))
+                expression.splice(initialIndex + 1, 1)
+                expression.splice(initialIndex - 1, 1)
+            }
+        })
+        expression.forEach((y) => {
+            let initialIndex = expression.indexOf(y)
+            if (y === '+') {
+                expression[initialIndex] = "" + (Number(expression[initialIndex - 1]) + Number(expression[initialIndex + 1]))
+                expression.splice(initialIndex + 1, 1)
+                expression.splice(initialIndex - 1, 1)
+            } if (y === '-') {
+                expression[initialIndex] = "" + (Number(expression[initialIndex - 1]) - Number(expression[initialIndex + 1]))
+                expression.splice(initialIndex + 1, 1)
+                expression.splice(initialIndex - 1, 1)
+            }
+        })
+    }
+    updateInterface()
+}
+
+function updateInterface() {
+    console.log(expression)
+    display.innerHTML = expression.join(' ')
+}
+
