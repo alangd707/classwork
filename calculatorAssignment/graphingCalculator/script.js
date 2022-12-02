5f2441a57b94dbe89e17da949748c32f1b5a83a0
alangd707 <111930791+alangd707@users.noreply.github.com>
2022-12-01 22:20:21 -0700

    Tes

--- calculatorAssignment/graphingCalculator/script.js
+++ calculatorAssignment/graphingCalculator/script.js
@@ -3,24 +3,24 @@ const ctx = canvas.getContext('2d');
 
 ctx.scale(1,-1);
 ctx.translate(canvas.width/2, -canvas.height/2);
-ctx.lineWidth = 2;
-ctx.moveTo(0,-250);
-ctx.lineTo(0,250)
-ctx.stroke();
-ctx.moveTo(-250,0);
-ctx.lineTo(250,0)
-ctx.stroke();
 
-// ctx.beginPath();
-// ctx.moveTo(0, -250);
-// ctx.quadraticCurveTo(50, 250, 100, -250);
-// ctx.stroke();
+function strokeBackground() {
+    ctx.strokeStyle = 'black';
+    ctx.lineWidth = 2;
+    ctx.moveTo(0,-250);
+    ctx.lineTo(0,250)
+    ctx.stroke();
+    ctx.moveTo(-250,0);
+    ctx.lineTo(250,0)
+    ctx.stroke();
+}
 
 function strokeParabola() {
     ctx.beginPath();
     ctx.fillStyle = 'red';
-    for (let i = -75; i <= 75; i = i + 25) {
-        ctx.fillRect(i,((i/25)**2) * 25,2,2)
+    for (let i = -10; i <= 10; i += 0.01 ) {
+        ctx.fillRect(i * 25, i**2 * 25, 1, 1)
     }
 }
+strokeBackground()
 strokeParabola()
