6b35e6483e0d0e8b981db891f051f7656fafd6d2
alangd707 <alangd707@west-mec.org>
2022-12-02 18:21:08 -0700

    Semi-functioning sin calculator

--- calculatorAssignment/graphingCalculator/script.js
+++ calculatorAssignment/graphingCalculator/script.js
@@ -12,14 +12,28 @@ function strokeBackground() {
     ctx.stroke();
     ctx.moveTo(-250,0);
     ctx.lineTo(250,0)
-    ctx.stroke();
+    ctx.stroke(); 
+    for (let i = -10; i < 20; i += 2) {
+        if (i==10) {continue};
+        ctx.strokeStyle = 'gray'
+        ctx.lineWidth = 1;
+        ctx.beginPath();
+        ctx.moveTo(i * 25, -250);
+        ctx.lineTo(i * 25, 250);
+        ctx.stroke();
+        ctx.beginPath();
+        ctx.moveTo(-250, i * 25);
+        ctx.lineTo(250, i * 25);
+        ctx.stroke();
+    }
 }
 
 function strokeParabola() {
     ctx.beginPath();
     ctx.fillStyle = 'red';
-    for (let i = -10; i <= 10; i += 0.01 ) {
-        ctx.fillRect(i * 25, i**2 * 25, 1, 1)
+    let y
+    for (let x = -10; x <= 10; x += 0.001 ) {
+        ctx.fillRect(x * 25, 2 * Math.sin(x) * 25, 1, 1)
     }
 }
 strokeBackground()
