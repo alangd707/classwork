40e3576ebb31c791734623a260cc5c2d1a5f401e
alangd707 <alangd707@west-mec.org>
2022-12-05 18:20:27 -0700

    Changed graph interface
    - Accurate graph interface

--- calculatorAssignment/graphingCalculator/script.js
+++ calculatorAssignment/graphingCalculator/script.js
@@ -5,36 +5,62 @@ ctx.scale(1,-1);
 ctx.translate(canvas.width/2, -canvas.height/2);
 
 function strokeBackground() {
-    ctx.strokeStyle = 'black';
-    ctx.lineWidth = 2;
-    ctx.moveTo(0,-250);
-    ctx.lineTo(0,250)
-    ctx.stroke();
-    ctx.moveTo(-250,0);
-    ctx.lineTo(250,0)
-    ctx.stroke(); 
     for (let i = -10; i < 20; i += 2) {
-        if (i==10) {continue};
-        ctx.strokeStyle = 'gray'
-        ctx.lineWidth = 1;
         ctx.beginPath();
+        if (i==0) {
+            ctx.strokeStyle = 'black';
+            ctx.lineWidth = 2;
+        } else if (i%4 == 0) {
+            ctx.lineWidth = 2;
+        } else {
+            ctx.strokeStyle = 'gray';
+            ctx.lineWidth = .5;
+        }
         ctx.moveTo(i * 25, -250);
         ctx.lineTo(i * 25, 250);
         ctx.stroke();
-        ctx.beginPath();
         ctx.moveTo(-250, i * 25);
         ctx.lineTo(250, i * 25);
         ctx.stroke();
     }
 }
 
-function strokeParabola() {
-    ctx.beginPath();
-    ctx.fillStyle = 'red';
-    let y
-    for (let x = -10; x <= 10; x += 0.001 ) {
-        ctx.fillRect(x * 25, 2 * Math.sin(x) * 25, 1, 1)
+function strokeNumbers() {
+    ctx.scale(1,-1)
+    ctx.textAlign = 'center'
+    ctx.fillStyle = 'black';
+    for (let i = -10; i < 20; i += 2) {
+        ctx.fillText(-i, 5, i * 25)
+        ctx.fillText(i, i * 25, -3)
     }
 }
+
+function strokeFunction() {
+    ctx.beginPath();
+    ctx.fillStyle = 'red';
+    ctx.fontSize = '4'
+    for (let x = -10; x <= 10; x += 0.001 ) {
+        if ((x**2 <= 10)) {
+            ctx.fillRect(x * 25, (x**2) * 25, 1, 1)
+        }
+    }
+}
+
+function getUserInput() {
+    let userInput = document.getElementById('user-input').value.replaceAll(' ', '')
+    for (let i = 0; i < userInput.length; i++) {
+        if (userInput[i] === '+' || userInput[i] === '-' || userInput[i] === '*' || userInput[i] === '/') {
+            let operator = userInput[i];
+            if (typeof(userInput) == 'string') {
+                userInput.split(String(operator))
+                console.log(userInput)
+                userInput.splice(1, 0, operator)
+            }
+
+        }
+    }
+    console.log(userInput)
+}
 strokeBackground()
-strokeParabola()
+strokeFunction()
+strokeNumbers() 
